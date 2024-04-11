f2ecdd1caeb58bb5fe326f325aa24b7bcd497c24
Jynn 🐈 <catsriding@gmail.com>
2024-04-12 02:21:02 +0900

    🎉 init: initialize comments system project

---
+++ src/main/java/app/catsriding/dev/common/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package app.catsriding.dev.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
